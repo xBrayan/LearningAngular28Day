efea509b85ee5d5357a34ef27390940eec62badc
Brayan <brayanbernabe97@outlook.com>
2022-02-12 18:06:23 -0500

    ciclo de vida de un componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
   name!: string;
   //name = "Domini";
   cities = ['Twinza', '1ero de Mayo', 'Eloy Alfaro', 'Las Peñas'];
   title = 'Día 4 del reto';
   url = 'https://res.cloudinary.com/practicaldev/image/fetch/s--9O4-RiZT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://miro.medium.com/max/990/1%2AOc2PsJ-QKOUG2I8J3HNmWQ.png';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Change->',changes)
+  }
+
+  ngOnInit(): void {
+    console.log('OnInit ->')
+  }
+
+  ngOnDestroy(): void {
+    console.log('Destroy')
+  }
 }
