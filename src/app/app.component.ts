a430e0ca62d8eda53c20dcf9b3ac46fc7b6fe275
Brayan <brayanbernabe97@outlook.com>
2022-02-12 19:00:26 -0500

    Event binding - seleccion de menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,27 @@ export class AppComponent implements OnChanges, OnInit, OnDestroy {
   cities = ['Twinza', '1ero de Mayo', 'Eloy Alfaro', 'Las Peñas'];
   title = 'Día 4 del reto';
   url = 'https://res.cloudinary.com/practicaldev/image/fetch/s--9O4-RiZT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://miro.medium.com/max/990/1%2AOc2PsJ-QKOUG2I8J3HNmWQ.png';
+  selection!:String;
+
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('Change->',changes)
+    console.log('Change->',changes);
   }
 
   ngOnInit(): void {
-    console.log('OnInit ->')
+    console.log('OnInit ->');
   }
 
   ngOnDestroy(): void {
-    console.log('Destroy')
+    console.log('Destroy');
+  }
+
+  onCityClicked(city: string):void{
+    console.log('City ->' , city);
+    this.selection = city;
+  }
+
+  onClear():void{
+    this.selection = '';
   }
 }
